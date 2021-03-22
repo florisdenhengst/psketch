0b8b9c4eac572dd6d4947fa32885f316d9c971bf
Floris den Hengst <florisdenhengst@gmail.com>
2021-03-22 20:10:27 +0100

    Fix lightworld domain knowledge

--- worlds/domain_knowledge.py
+++ worlds/domain_knowledge.py
@@ -1,21 +1,24 @@
 from worlds import light
 from worlds import craft
 
+import logging
+
 class LightWorldDomainKnowledge():
-    def __init__(self, goals):
+    def __init__(self, goals, cookbook):
         self.prev_action_label = None
         self.prev_observation = None
         # a sequence of symbolic high-level subgoals
         self.goals = goals
         self.current_goal_i = 0
     
-    def tick(self, observation, action):
+    def tick(self, state, action):
+        observation = state.features()
         subgoal_met = False
         #last_goal = self.current_goal_i == len(self.goals)
         if self.prev_action_label != None:
             subgoal_met = self.subgoal_met(observation)
             if subgoal_met:
-                self.current_goal_i = min(len(self.goals), self.current_goal_i + 1)
+                self.current_goal_i = min(len(self.goals) - 1, self.current_goal_i + 1)
         self.prev_observation = observation
         self.prev_action_label = action
         return subgoal_met #and not last_goal
@@ -26,20 +29,63 @@ class LightWorldDomainKnowledge():
     def subgoal_met(self, observation):
         if not self.in_door(self.prev_observation) or self.in_door(observation):
             return False
-        elif self.goals[self.current_goal_i] == 'l' and self.prev_action_label == light.LEFT:
+        elif self.goals[self.current_goal_i] == 'left' and self.prev_action_label == light.LEFT:
             return True
-        elif self.goals[self.current_goal_i] == 'u' and self.prev_action_label == light.UP:
+        elif self.goals[self.current_goal_i] == 'up' and self.prev_action_label == light.UP:
             return True
-        elif self.goals[self.current_goal_i] == 'd' and self.prev_action_label == light.DOWN:
+        elif self.goals[self.current_goal_i] == 'down' and self.prev_action_label == light.DOWN:
             return True
-        elif self.goals[self.current_goal_i] == 'r' and self.prev_action_label == light.RIGHT:
+        elif self.goals[self.current_goal_i] == 'right' and self.prev_action_label == light.RIGHT:
             return True
+        return False
+
+class CraftWorldDomainKnowledge():
+    def __init__(self, goals, cookbook):
+        self.prev_action_label = None
+        self.prev_observation = None
+        # a sequence of symbolic high-level subgoals
+        self.goals = goals
+        self.current_goal_i = 0
+        self.cookbook = cookbook
+
+    def tick(self, state, action):
+        subgoal_met = self.subgoal_met(state, action)
+        #last_goal = self.current_goal_i == len(self.goals)
+        if subgoal_met:
+            self.current_goal_i = min(len(self.goals), self.current_goal_i + 1)
+        self.prev_state = state
+        self.prev_action_label = action
+        return subgoal_met #and not last_goal
+
+    def check_inventory(self, state, keyword):
+        # TODO FdH:
+        #  * Change > 0.0 to == 1.0?
+        return state.inventory[self.cookbook.index[keyword]] > 0.0
+
+    def subgoal_met(self, state, action):
+        subgoal = self.goals[self.current_goal_i]
+        if subgoal == 'get_grass':
+            return self.check_inventory(state, 'wood')
+        if subgoal == 'get_grass':
+            return self.check_inventory(state, 'grass')
+        if subgoal == 'get_iron':
+            return self.check_inventory(state, 'iron')
+        if subgoal == 'get_axe':
+            return self.check_inventory(state, 'axe')
+        if subgoal == 'get_gold':
+            return self.check_inventory(state, 'gold')
+        if subgoal == 'get_gem':
+            return self.check_inventory(state, 'gem')
+        if subgoal[:4] == 'make':
+            logging.debug("subgoal make: {}".format(subgoal))
+            return state.at_workshop(subgoal[-1]) and self.prev_action_label == craft.USE
         return False
 
 def domain_model(config):
     world = config.world.name
     world_models = {
             'LightWorld': LightWorldDomainKnowledge,
+            'CraftWorld': CraftWorldDomainKnowledge,
             }
     return world_models[world]
 
