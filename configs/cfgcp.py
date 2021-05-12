6a4dd75a8b2b326ad40021f558192e2fb7332492
Floris den Hengst <florisdenhengst@gmail.com>
2021-05-12 19:22:24 +0200

    Add mamc configs

--- configs/cfgcp.py
+++ configs/cfgcp.py
@@ -1,16 +1,37 @@
 import yaml
 import glob
 
-files = glob.glob('*.yaml')
+files = glob.glob('craft*modular-[0-9]-forcestop-*.yaml')
+
+HINT_FILE_LOC = 'resources/craft/'
+tasks = ['axe',
+        'bed',
+        'bridge',
+        'cloth',
+        'gem',
+        'gold',
+        'plank',
+        'rope',
+        'shears',
+        'stick',
+]
+task_hints = [t + '.yaml' for t in tasks]
 
 cfgs = []
 for f in files:
     with open(f) as fp:
         cfgs.append(yaml.load(fp))
+
 for cfg in cfgs:
-    to_name = cfg['name'].replace('modular', 'mamc')
-    cfg['model']['name'] = 'ModularActorModularCriticModel'
-    with open(to_name, 'w+') as fp:
-        yaml.dump(cfg, fp)
+    if cfg['seed'] == 1.0:
+        continue
+    print(cfg['name'])
+    cfgname = cfg['name']
+    for ti, task in enumerate(tasks):
+        cfg['name'] = cfgname + '-' + task
+        cfg['trainer']['hints'] = HINT_FILE_LOC + task_hints[ti]
+        fname = '/tmp/' + cfg['name'] + '.yaml'
+        with open(fname, 'w+') as fp:
+            yaml.dump(cfg, fp)
 
 
